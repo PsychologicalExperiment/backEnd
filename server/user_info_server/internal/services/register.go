40284c92f69af75fc6594e95c2db287ee2339250
caijh23 <caijh23@gmail.com>
2022-04-09 16:46:26 +0800

    commit service code

--- server/user_info_server/internal/services/register.go
+++ server/user_info_server/internal/services/register.go
@@ -1,16 +1,15 @@
-package register
+package services
 
 import (
 	"context"
 
-	commonPb "github.com/PsychologicalExperiment/backEnd/api/api_common"
 	userInfoPb "github.com/PsychologicalExperiment/backEnd/api/user_info_server"
 )
 
-func Register(
+func (u *UserInfoServerImpl) Register(
 	ctx context.Context,
 	req *userInfoPb.RegisterReq,
 ) (*userInfoPb.RegisterRsp, error) {
-	commonPb.CommonHead
+
 	return nil, nil
 }
