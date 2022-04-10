9822dbf1e943d72fd423935628bf0d9cb80fdaf5
caijh23 <caijh23@gmail.com>
2022-04-10 17:43:58 +0800

    edit code for user info server

--- server/user_info_server/go.mod
+++ server/user_info_server/go.mod
@@ -4,8 +4,9 @@ go 1.16
 
 require (
 	github.com/PsychologicalExperiment/backEnd v0.0.0-20220405085841-41916c8ea5cb
+	github.com/go-redis/redis/v8 v8.11.5
 	google.golang.org/grpc v1.45.0
-	gopkg.in/yaml.v2 v2.2.3
+	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.3.3
 	gorm.io/gorm v1.23.4
 )
--- server/user_info_server/internal/services/userInfo.go
+++ server/user_info_server/internal/services/userInfo.go
@@ -1,18 +1,43 @@
 package services
 
 import (
+	"github.com/go-redis/redis/v8"
+	"google.golang.org/grpc/grpclog"
 	"gorm.io/gorm"
 )
 
 type UserInfoServerImpl struct {
-	sqlConn *gorm.DB
+	sqlConn  *gorm.DB
+	redisCli *redis.Client
 }
 
 type userInfo struct {
 	gorm.Model         // ID,CreateAt,UpdateAt,DeletedAt
 	Email       string `gorm:"type:varchar(100);unique_index"` //唯一索引
 	PhoneNumber string `gorm:"type:varchar(20);unique"`
-	Uin         string `gorm:"unique;not null"` //唯一并且不为空
 	UserName    string `gorm:"type:varchar(10)"`
 	Gender      uint32 `gorm:"type:tinyint(3);not null"`
+}
+
+func (u *UserInfoServerImpl) insertUserInfo(
+	user *userInfo,
+) error {
+	res := u.sqlConn.Create(user)
+	if res.Error != nil {
+		grpclog.Errorf("insert into db failed, error: %+v, userInfo: %+v", res.Error, user)
+		return res.Error
+	}
+	return nil
+}
+
+func (u *UserInfoServerImpl) isEmailUsed(
+	email string,
+) (bool, error) {
+	users := []userInfo{}
+	res := u.sqlConn.Where("email = ?", email).Find(&users)
+	if res.Error != nil {
+		grpclog.Errorf("read db failed, error: %+v", res.Error)
+		return false, res.Error
+	}
+	return len(users) > 0, nil
 }
