9551049ad237815dc24dc546fc7466c68bf0ac3c
jacenchen228 <jacenchen228@gmail.com>
2022-06-12 18:26:12 +0800

    增加用户接口

--- server/user_info_server/go.mod
+++ server/user_info_server/go.mod
@@ -1,16 +1,16 @@
 module github.com/PsychologicalExperiment/backEnd/server/user_info_server
 
-replace github.com/PsychologicalExperiment/backEnd/api/user_info_server => ../../api/user_info_server
+//replace github.com/PsychologicalExperiment/backEnd/api/user_info_server => ../../api/user_info_server
 
 go 1.16
 
 require (
-	github.com/PsychologicalExperiment/backEnd v0.0.0-20220612081424-2db32d97b7ef
+	github.com/PsychologicalExperiment/backEnd v0.0.0-20220612085330-3e6c942d2599
 	//github.com/PsychologicalExperiment/backEnd/api/user_info_server v0.0.0-20220522145057-7affc71739be
 	github.com/PsychologicalExperiment/backEnd/util v0.0.0-20220410153153-365d8503d6fa
 	github.com/go-redis/redis/v8 v8.11.5
 	google.golang.org/grpc v1.45.0
+	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.3.3
 	gorm.io/gorm v1.23.4
-	gopkg.in/yaml.v2 v2.4.0
 )
---
+++ server/user_info_server/internal/services/get_user_info.go
@@ -0,0 +1,61 @@
+package services
+
+import (
+	userInfoPb "github.com/PsychologicalExperiment/backEnd/api/user_info_server"
+	"github.com/PsychologicalExperiment/backEnd/server/user_info_server/internal/services/serverErr"
+	"google.golang.org/grpc/grpclog"
+	"strings"
+)
+
+package services
+
+import (
+"context"
+userInfoPb "github.com/PsychologicalExperiment/backEnd/api/user_info_server"
+"github.com/PsychologicalExperiment/backEnd/server/user_info_server/internal/services/serverErr"
+"google.golang.org/grpc/grpclog"
+"strings"
+)
+
+func (u *UserInfoServerImpl) G(
+	ctx context.Context,
+	req *userInfoPb.,
+) (*userInfoPb.LoginRsp, error) {
+	grpclog.Infof("req: %+v", req)
+	var user []userInfo
+	var err error
+	if req.Email != "" {
+		user, err = u.getUserInfosByKey(searchKeyEmail, req.Email)
+	} else {
+		user, err = u.getUserInfosByKey(searchKeyPhoneNumber, req.PhoneNumber)
+	}
+	// 用户未找到
+	if err != nil || len(user) == 0 {
+		grpclog.Errorf("get user info failed, req: %+v", req)
+		resp := &userInfoPb.LoginRsp{
+			CommonRsp: serverErr.CommonRsp(serverErr.New(serverErr.ErrorUserNotFound)),
+		}
+		return resp, nil
+	}
+	// 密码错误
+	if strings.Compare(user[0].Password, req.Password) != 0 {
+		grpclog.Errorf("password not right, req: %+v", req)
+		resp := &userInfoPb.LoginRsp{
+			CommonRsp: serverErr.CommonRsp(serverErr.New(serverErr.ErrorPasswordNotRight)),
+		}
+		return resp, nil
+	}
+	// 验证成功
+	resp := &userInfoPb.LoginRsp{
+		CommonRsp: serverErr.CommonRsp(serverErr.New(serverErr.OKCode)),
+		UserInfo: &userInfoPb.UserInfo{UserType: userInfoPb.UserType(user[0].UserType), Email: user[0].Email, PhoneNumber: user[0].PhoneNumber,
+			UserName: user[0].UserName, Gender: userInfoPb.GenderType(user[0].Gender), Extra: user[0].Extra},
+	}
+	return resp, nil
+}
+
+func (u *UserInfoServerImpl) loginParamCheck(
+	req *userInfoPb.LoginReq,
+) error {
+	return nil
+}
