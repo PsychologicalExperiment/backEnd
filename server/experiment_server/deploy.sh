10342edd5fde32ba417483cf76811a086d8049ac
bravolu <bravolu@tencent.com>
2023-01-17 19:31:33 +0800

    update deploy.sh

---
+++ server/experiment_server/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo rm -rf /data/psychology/experiment_server/experiment_server &> /dev/null && \
+cp bin/experiment_server /data/psychology/experiment_server/experiment_server && \
+cp -r config /data/psychology/experiment_server/config && \
+cp restart.sh /data/psychology/experiment_server/ && \
+cd /data/psychology/experiment_server && sh restart.sh experiment_server &> /dev/null 2>&1 &
+
+
+ssh ahan "sudo rm -rf /data/psychology/experiment_server/experiment_server &> /dev/null" && \
+scp bin/experiment_server ahan:/data/psychology/experiment_serverexperiment_server && \
+scp -r config ahan:/data/psychology/experiment_server/config && \
+scp restart.sh ahan:/data/psychology/experiment_server/ && \
+ssh ahan "cd /data/psychology/experiment_server && sh restart.sh &> /dev/null 2>&1 &"
+
+
+ssh musk "sudo rm -rf /data/psychology/experiment_server/experiment_server &> /dev/null" && \
+scp bin/experiment_server musk:/data/psychology/experiment_server/experiment_server && \
+scp -r config musk:/data/psychology/experiment_server/config && \
+scp restart.sh musk:/data/psychology/experiment_server/ && \
+ssh musk "cd /data/psychology/experiment_server && sh restart.sh &> /dev/null 2>&1 &"
--- server/experiment_server/restart.sh
+++ server/experiment_server/restart.sh
@@ -1,2 +1,2 @@
-ps -ef | grep experiment_server | grep -v grep | awk  '{print $2}'| xargs kill -9
+ps -ef | grep experiment_server | grep -v grep | awk  '{print $2}'| xargs kill -9 &> /dev/null 2>&1 &
 nohup ./experiment_server &
