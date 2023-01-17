cbb663e5d02ab0344eb184fad1ca1f1dc508f7b3
bravolu <bravolu@tencent.com>
2023-01-17 22:00:19 +0800

    update

--- server/experiment_server/deploy.sh
+++ server/experiment_server/deploy.sh
@@ -7,7 +7,7 @@ cd /data/psychology/experiment_server && sh restart.sh &
 
 
 ssh ahan "rm -rf /data/psychology/experiment_server/experiment_server &> /dev/null" &&
-scp bin/experiment_server ahan:/data/psychology/experiment_serverexperiment_server &&
+scp bin/experiment_server ahan:/data/psychology/experiment_server/experiment_server &&
 scp -r config ahan:/data/psychology/experiment_server/config &&
 scp restart.sh ahan:/data/psychology/experiment_server/ &&
 ssh ahan "cd /data/psychology/experiment_server && sh restart.sh &> /dev/null 2>&1 &"
